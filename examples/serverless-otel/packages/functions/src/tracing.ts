68ed2bac4121fd889dc6c68fefd6e55fef745d50
Ankcorn <thomasankcorn@gmail.com>
2024-03-22 17:23:47 +0000

    tidyup

--- examples/serverless-otel/packages/functions/src/tracing.ts
+++ examples/serverless-otel/packages/functions/src/tracing.ts
@@ -3,7 +3,7 @@ import { AwsInstrumentation } from '@opentelemetry/instrumentation-aws-sdk';
 import { withOpenTelemetry } from '../../../../../src/lambda';
 
 new BaselimeSDK({
-    baselimeKey: '***REMOVED***', collectorUrl: 'https://otel.baselime.cc/v1', serverless: false, instrumentations: [
+    baselimeKey: '', collectorUrl: 'https://otel.baselime.cc/v1', serverless: false, instrumentations: [
       new AwsInstrumentation({
         suppressInternalInstrumentation: true,
       }),
